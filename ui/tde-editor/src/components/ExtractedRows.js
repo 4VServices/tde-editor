60df53e4cd6e20fbb245aa035f716db55ba863af
PhongHo93 <82865192+PhongHo93@users.noreply.github.com>
2022-08-23 13:39:27 -0400

    Display extraction view results

--- ui/tde-editor/src/components/ExtractedRows.js
+++ ui/tde-editor/src/components/ExtractedRows.js
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import './ExtractedRows.css';
 import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
 
-class ExtractedRows extends React.Component {
-  render() {
-    return (
-      <Container className="ExtractedRows">
-        <h3>ExtractedRows</h3>
-        <div>No rows found</div>
-      </Container>
-    );
-  }
-}
+const ExtractedRows = ({ extractedData }) => {
+  const extractedRows = useMemo(() => {
+    if (!extractedData) return [];
+    let result = [];
+    Object.keys(extractedData).forEach((key) => {
+      const data = extractedData[key];
+      data?.forEach((dd) => {
+        if (dd.row?.data) {
+          result.push({
+            ...dd.row.data,
+            key: key
+          });
+        }
+      });
+    });
+
+    return result;
+  }, [extractedData]);
+
+  return (
+    <Container className="ExtractedRows">
+      <h3>ExtractedRows</h3>
+      {!extractedData && <div>No rows found</div>}
+      {extractedRows?.length > 0 && (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>document</th>
+              <th>date</th>
+              <th>league</th>
+            </tr>
+          </thead>
+          <tbody>
+            {extractedRows.map((rowData) => {
+              return (
+                <tr key={rowData.id}>
+                  <td>{rowData.id}</td>
+                  <td>{rowData.key}</td>
+                  <td>{rowData.date}</td>
+                  <td>{rowData.league}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      )}
+    </Container>
+  );
+};
 
 export default ExtractedRows;
--- ui/tde-editor/src/components/ExtractedTriples.js
+++ ui/tde-editor/src/components/ExtractedTriples.js
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Container from 'react-bootstrap/esm/Container';
 import './ExtractedTriples.css';
+import Stack from 'react-bootstrap/Stack';
 
-class Triples extends React.Component {
-  render() {
-    return (
-      <Container className="extractedTriples">
-        <h3>Extracted Triples</h3>
-        <div>No rows found</div>
-      </Container>
-    );
-  }
-}
+const ExtractedTriples = ({ extractedData }) => {
+  const extractedTriples = useMemo(() => {
+    if (!extractedData) return [];
+    let result = [];
+    Object.keys(extractedData).forEach((key) => {
+      const data = extractedData[key];
+      data?.forEach((dd) => {
+        if (dd.triple) {
+          result.push({
+            ...dd.triple,
+            key: key
+          });
+        }
+      });
+    });
 
-export default Triples;
+    return result;
+  }, [extractedData]);
+
+  return (
+    <Container className="extractedTriples">
+      <h3>Extracted Triples</h3>
+      {!extractedTriples.length && <div>No rows found</div>}
+      {extractedTriples?.length > 0 && (
+        <Stack gap={3}>
+          {extractedTriples.map((triple, index) => {
+            return (
+              <div className="triple-container" key={`${triple.object}-${index}`}>
+                <Stack direction="horizontal" gap={3}>
+                  <div className="label">Subject:</div>
+                  <div>{triple.subject}</div>
+                </Stack>
+                <Stack direction="horizontal" gap={3}>
+                  <div className="label">Predicate:</div>
+                  <div>{triple.predicate}</div>
+                </Stack>
+                <Stack direction="horizontal" gap={3}>
+                  <div className="label">Object:</div>
+                  <div>{triple.object}</div>
+                </Stack>
+              </div>
+            );
+          })}
+        </Stack>
+      )}
+    </Container>
+  );
+};
+
+export default ExtractedTriples;
