50335fc7379f9ad9b0f7a8f6644953c4ea5023d8
David Cassel <dmcassel@gmail.com>
2022-09-14 17:07:08 -0400

    Change names; display results with column specs

--- ui/tde-editor/src/components/Views.js
+++ ui/tde-editor/src/components/Views.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import ViewRow from './ViewRow';
-import ExtractedRows from './ExtractedRows';
+import View from './View';
 import { Group } from './Group';
 import { Button } from './Button';
 import { FlexBox } from './Box';
 
-const ViewRows = ({ extractedData, rowsSpec, onRowChange, onRowDelete, onAddRow }) => {
+const Views = ({ extractedData, rowsSpec, onRowChange, onRowDelete, onAddRow }) => {
   return (
-    <Group title="View Rows">
+    <Group title="Views">
       <FlexBox gap="2rem" flexDirection="column" alignItems="stretch">
         {rowsSpec &&
           rowsSpec.map((row, index) => (
-            <ViewRow viewRow={row} key={index} index={index} onRowChange={onRowChange} onRowDelete={onRowDelete} />
+            <View
+              view={row}
+              key={index}
+              index={index}
+              onRowChange={onRowChange}
+              onRowDelete={onRowDelete}
+              extractedData={extractedData}
+            />
           ))}
       </FlexBox>
 
       <FlexBox gap="1rem" margin="1rem 0 3rem">
         <Button onClick={onAddRow}>Add View</Button>
       </FlexBox>
-
-      <ExtractedRows extractedData={extractedData} rowsSpec={rowsSpec} />
     </Group>
   );
 };
 
-export default ViewRows;
+export default Views;
