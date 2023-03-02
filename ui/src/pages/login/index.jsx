2b6b7047ac640052d61d46ddbafb47553f9a8a19
PhongHo93 <82865192+PhongHo93@users.noreply.github.com>
2023-03-03 02:34:04 +0700

    display header content in logoin page

--- ui/src/pages/login/index.jsx
+++ ui/src/pages/login/index.jsx
@@ -4,6 +4,9 @@ import useUser from 'hooks/useUser';
 import { Button, Typography, Form, Input } from 'antd';
 import styled from 'styled-components';
 import { FlexBox } from 'components/Box';
+import { H1, H3 } from 'components/common';
+import logo from '../../logo.png';
+
 const { Text } = Typography;
 
 const Root = styled.div`
@@ -11,23 +14,43 @@ const Root = styled.div`
   justify-content: center;
   padding-top: 25vh;
   min-height: 100vh;
-  background-color: #f2f2ee;
+  background-color: #fbfbfb;
 `;
 
+const Header = styled.div`
+  position: absolute;
+  max-width: 1280px;
+  margin-left: auto;
+  margin-right: auto;
+  align-items: center;
+  margin: 2rem 0px;
+  width: 100%;
+  gap: 2rem;
+  top: 0;
+  display: flex;
+`;
+
+const Img = styled.img`
+  height: 10vmin;
+  pointer-events: none;
+`;
 const LoginPage = () => {
   const [error, setError] = useState();
   const { setUser } = useUser();
 
-  const onFinish = useCallback(async (values) => {
-    setError(false);
-    try {
-      const res = await login({ username: values.username, password: values.password });
-      if (res.data) setUser(res.data);
-    } catch (e) {
-      setError(true);
-      console.log('login error:', e);
-    }
-  }, [setUser]);
+  const onFinish = useCallback(
+    async (values) => {
+      setError(false);
+      try {
+        const res = await login({ username: values.username, password: values.password });
+        if (res.data) setUser(res.data);
+      } catch (e) {
+        setError(true);
+        console.log('login error:', e);
+      }
+    },
+    [setUser]
+  );
 
   const onFinishFailed = (errorInfo) => {};
 
@@ -37,6 +60,18 @@ const LoginPage = () => {
 
   return (
     <Root>
+      <Header>
+        <FlexBox justifyContent="space-between" width="100%">
+          <FlexBox gap="2rem">
+            <Img src={logo} className="App-logo" alt="logo" />
+            <H1>TDE Template Editor</H1>
+          </FlexBox>
+          <H3>
+            Built by <a href="http://4VServices.com">4V Services</a>
+          </H3>
+        </FlexBox>
+      </Header>
+
       <Form
         name="basic"
         layout="vertical"
