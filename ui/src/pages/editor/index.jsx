0d5ec45a59eb22c042084ff5a0ac423419456207
PhongHo93 <82865192+PhongHo93@users.noreply.github.com>
2023-03-02 19:56:01 +0200

    in header, show username & logout btn

--- ui/src/pages/editor/index.jsx
+++ ui/src/pages/editor/index.jsx
@@ -1,9 +1,11 @@
-
 import Editor from 'components/Editor';
 import styled from 'styled-components';
 import { FlexBox } from 'components/Box';
-import { H1, H3 } from 'components/common';
+import { B2Med, H1, H3 } from 'components/common';
 import logo from '../../logo.png';
+import useUser from 'hooks/useUser';
+import { Button } from 'components/Button';
+import { LogoutOutlined } from '@ant-design/icons';
 
 const Container = styled.div`
   max-width: 1280px;
@@ -25,29 +27,36 @@ const Img = styled.img`
 `;
 
 const EditorPage = () => {
-    return (
-        <Container>
-            <FlexBox width="100%" gap="2rem" margin="2rem 0">
-                <div className="left">
-                    <FlexBox justifyContent="space-between">
-                        <Img src={logo} className="App-logo" alt="logo" />
-                    </FlexBox>
-                </div>
-                <div className="right">
-                    <FlexBox justifyContent="space-between">
-                        <H1>TDE Template Editor</H1>
-                        <H3>
-                            Built by <a href="http://4VServices.com">4V Services</a>
-                        </H3>
-                    </FlexBox>
-                </div>
-            </FlexBox>
+  const { user, logout } = useUser();
+  return (
+    <Container>
+      <FlexBox width="100%" gap="2rem" margin="2rem 0">
+        <div className="left">
+          <FlexBox justifyContent="space-between">
+            <Img src={logo} className="App-logo" alt="logo" />
+          </FlexBox>
+        </div>
+        <div className="right">
+          <FlexBox justifyContent="space-between">
+            <H1>TDE Template Editor</H1>
+            <H3>
+              Built by <a href="http://4VServices.com">4V Services</a>
+              <FlexBox gap="1rem">
+                <B2Med>{user.username}</B2Med>
+                <Button type="link" onClick={logout} icon={<LogoutOutlined />}>
+                  Logout
+                </Button>
+              </FlexBox>
+            </H3>
+          </FlexBox>
+        </div>
+      </FlexBox>
 
-            <FlexBox width="100%">
-                <Editor />
-            </FlexBox>
-        </Container>
-    )
-}
+      <FlexBox width="100%">
+        <Editor />
+      </FlexBox>
+    </Container>
+  );
+};
 
-export default EditorPage
+export default EditorPage;
