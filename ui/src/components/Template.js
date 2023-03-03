9dcec5bb5decc0d6b8630f1cfce77f3520f31c37
David Cassel <dmcassel@gmail.com>
2023-03-03 12:54:35 -0500

    Use value, not placeholder, for description

--- ui/src/components/Template.js
+++ ui/src/components/Template.js
@@ -17,13 +17,7 @@ const Template = ({
     <Group title="Template">
       <FlexBox flexDirection="column" gap="1rem" alignItems="stretch">
         <TextEdit label="URI" value={templateURI} onChangeDebounced={handleURIChange} />
-        <TextEdit
-          label="Description"
-          type="textarea"
-          rows={3}
-          placeholder={description}
-          onChange={handleDescriptionChange}
-        />
+        <TextEdit label="Description" type="textarea" rows={3} value={description} onChange={handleDescriptionChange} />
         <TextEdit label="Collection" value={collection} onChange={handleCollectionChange} />
         <TextEdit label="Directory" placeholder={''} onChange={() => {}} />
         <TextEdit label="Context" value={context} onChange={handleContextChange} />
