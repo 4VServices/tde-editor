f209866b86bfdd854d09954be6dfc6208bd13d8e
David Cassel <dmcassel@gmail.com>
2025-01-10 10:38:31 -0500

    WIP update

--- ui/src/components/Template.js
+++ ui/src/components/Template.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Group } from './Group';
 import { TextEdit } from './TextEdit';
 import { FlexBox } from './Box';
@@ -9,16 +9,31 @@ const Template = ({
   handleDescriptionChange,
   description,
   handleCollectionChange,
-  collection,
+  collections,
   handleContextChange,
   context
 }) => {
+  console.log(`Template; collections=${JSON.stringify(collections)}`);
+  const [joinedCollections, setJoinedCollections] = useState(collections.join('\n'));
+
+  const handleCollectionInputChange = (event) => {
+    console.log(`handleCollectionInputChange: event=${JSON.stringify(event)}`);
+    setJoinedCollections(event);
+    handleCollectionChange(event.split('\n'));
+  };
+
   return (
     <Group title="Template">
       <FlexBox flexDirection="column" gap="1rem" alignItems="stretch">
         <TextEdit label="URI" value={templateURI} onChangeDebounced={handleURIChange} />
         <TextEdit label="Description" type="textarea" rows={3} value={description} onChange={handleDescriptionChange} />
-        <TextEdit label="Collection" value={collection} onChange={handleCollectionChange} />
+        <TextEdit
+          label="Collections"
+          type="textarea"
+          rows={3}
+          value={joinedCollections}
+          onChange={handleCollectionInputChange}
+        />
         <TextEdit label="Directory" placeholder={''} onChange={() => {}} />
         <TextEdit label="Context" value={context} onChange={handleContextChange} />
       </FlexBox>
